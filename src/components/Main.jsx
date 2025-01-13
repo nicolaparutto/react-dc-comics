01627242c524dc9992594cf5e311e34e1480b762
nicolaparutto <nicolaparutto2003@gmail.com>
2025-01-13 18:07:21 +0100

    feat: add main css style

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,29 +1,33 @@
 const Main = () =>{
    return(
-      <main className="container">
+      <main>
          <section className="content">
-            <span>--Content goes here--</span>
+            <div className="container">
+               <span>--Content goes here--</span>
+            </div>
          </section>
          <section className="dc_info">
-            <div className="col">
-               <img src="/img/buy-comics-digital-comics.png" alt="" />
-               <p>DIGITAL COMICS</p>
-            </div>
-            <div className="col">
-               <img src="/img/buy-comics-merchandise.png" alt="" />
-               <p>DC MERCHANDISE</p>
-            </div>
-            <div className="col">
-               <img src="/img/buy-comics-subscriptions.png" alt="" />
-               <p>SUBSCRIPTION</p>
-            </div>
-            <div className="col">
-               <img src="/img/buy-comics-shop-locator.png" alt="" />
-               <p>COMIC SHOP LOCATOR</p>
-            </div>
-            <div className="col">
-               <img src="/img/buy-dc-power-visa.svg" alt="" />
-               <p>DC POWER VISA</p>
+            <div className="row container">
+               <div className="col">
+                  <img src="/img/buy-comics-digital-comics.png" alt="" />
+                  <p>DIGITAL COMICS</p>
+               </div>
+               <div className="col">
+                  <img src="/img/buy-comics-merchandise.png" alt="" />
+                  <p>DC MERCHANDISE</p>
+               </div>
+               <div className="col">
+                  <img src="/img/buy-comics-subscriptions.png" alt="" />
+                  <p>SUBSCRIPTION</p>
+               </div>
+               <div className="col">
+                  <img src="/img/buy-comics-shop-locator.png" alt="" />
+                  <p>COMIC SHOP LOCATOR</p>
+               </div>
+               <div className="col">
+                  <img src="/img/buy-dc-power-visa.svg" alt="" />
+                  <p>DC POWER VISA</p>
+               </div>
             </div>
          </section>
       </main>
