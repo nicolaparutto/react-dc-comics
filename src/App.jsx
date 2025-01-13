e2cf5cd9f6f67fc27c8a65864f68b305efe92e4f
nicolaparutto <nicolaparutto2003@gmail.com>
2025-01-13 16:34:01 +0100

    feat: add components and project standardization

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
 const App = () => {
   return(
-    <h1>Hello World!</h1>
+    <>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </>
   )
 }
 
