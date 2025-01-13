d4bc8a14b01fa64d8a38d320b51247f4cc1fb903
nicolaparutto <nicolaparutto2003@gmail.com>
2025-01-13 17:46:16 +0100

    feat add main structure

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,32 @@
 const Main = () =>{
    return(
-      <main>Main</main>
+      <main className="container">
+         <section className="content">
+            <span>--Content goes here--</span>
+         </section>
+         <section className="dc_info">
+            <div className="col">
+               <img src="/img/buy-comics-digital-comics.png" alt="" />
+               <p>DIGITAL COMICS</p>
+            </div>
+            <div className="col">
+               <img src="/img/buy-comics-merchandise.png" alt="" />
+               <p>DC MERCHANDISE</p>
+            </div>
+            <div className="col">
+               <img src="/img/buy-comics-subscriptions.png" alt="" />
+               <p>SUBSCRIPTION</p>
+            </div>
+            <div className="col">
+               <img src="/img/buy-comics-shop-locator.png" alt="" />
+               <p>COMIC SHOP LOCATOR</p>
+            </div>
+            <div className="col">
+               <img src="/img/buy-dc-power-visa.svg" alt="" />
+               <p>DC POWER VISA</p>
+            </div>
+         </section>
+      </main>
    )
 }
 
