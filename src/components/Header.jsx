00e2e90cc7a46b48d46a01f9ed908c51aeec9e3c
nicolaparutto <nicolaparutto2003@gmail.com>
2025-01-13 16:56:49 +0100

    feat: add header structure & main css style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,25 @@
 const Header = () =>{
    return(
-      <header>Header</header>
+      <header>
+         <figure>
+         <img src="img/dc-logo.png" alt="Logo" />
+         </figure>
+         <nav>
+            <ul>
+               <li><a href="#">CHARACTERS</a></li>
+               <li><a href="#">COMICS</a></li>
+               <li><a href="#">MOVIES</a></li>
+               <li><a href="#">TV</a></li>
+               <li><a href="#">GAMES</a></li>
+               <li><a href="#">COLLECTIBLES</a></li>
+               <li><a href="#">VIDEOS</a></li>
+               <li><a href="#">FANS</a></li>
+               <li><a href="#">NEWS</a></li>
+               <li><a href="#">SHOP</a></li>
+            </ul>
+         </nav>
+      </header>
    )
 }
 
-export default Header
+export default Header;
