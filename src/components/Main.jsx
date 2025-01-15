b0e437f336465960816099272e2cf3fa75997043
nicolaparutto <nicolaparutto2003@gmail.com>
2025-01-15 12:08:40 +0100

    feat: add basic structure of cards section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,119 @@
 const Main = () =>{
    return(
       <main>
+         <section className="jumbotron">
+            <img src="/img/jumbotron.jpg" alt="" />
+         </section>
          <section className="content">
-            <div className="container">
-               <span>--Content goes here--</span>
+            <div className="container content_container">
+               <div className="content_title">
+                  <h3>CURRENT SERIES</h3>
+               </div>
+               <div className="cards_container">
+                  <div className="cards_row">
+                     <div className="cards_col">
+                        <div className="card">
+                           <div className="card_img">
+                              <img src="/img/dc-logo.png" alt="" />
+                           </div>
+                           <div className="card_title">
+                              <p>ciao ciao</p>
+                           </div>
+                        </div>
+                        <div className="card">
+                           <div className="card_img">
+                              <img src="/img/dc-logo.png" alt="" />
+                           </div>
+                           <div className="card_title">
+                              <p>ciao ciao</p>
+                           </div>
+                        </div>
+                        <div className="card">
+                           <div className="card_img">
+                              <img src="/img/dc-logo.png" alt="" />
+                           </div>
+                           <div className="card_title">
+                              <p>ciao ciao</p>
+                           </div>
+                        </div>
+                        <div className="card">
+                           <div className="card_img">
+                              <img src="/img/dc-logo.png" alt="" />
+                           </div>
+                           <div className="card_title">
+                              <p>ciao ciao</p>
+                           </div>
+                        </div>
+                        <div className="card">
+                           <div className="card_img">
+                              <img src="/img/dc-logo.png" alt="" />
+                           </div>
+                           <div className="card_title">
+                              <p>ciao ciao</p>
+                           </div>
+                        </div>
+                        <div className="card">
+                           <div className="card_img">
+                              <img src="/img/dc-logo.png" alt="" />
+                           </div>
+                           <div className="card_title">
+                              <p>ciao ciao</p>
+                           </div>
+                        </div>
+                        <div className="card">
+                           <div className="card_img">
+                              <img src="/img/dc-logo.png" alt="" />
+                           </div>
+                           <div className="card_title">
+                              <p>ciao ciao</p>
+                           </div>
+                        </div>
+                        <div className="card">
+                           <div className="card_img">
+                              <img src="/img/dc-logo.png" alt="" />
+                           </div>
+                           <div className="card_title">
+                              <p>ciao ciao</p>
+                           </div>
+                        </div>
+                        <div className="card">
+                           <div className="card_img">
+                              <img src="/img/dc-logo.png" alt="" />
+                           </div>
+                           <div className="card_title">
+                              <p>ciao ciao</p>
+                           </div>
+                        </div>
+                        <div className="card">
+                           <div className="card_img">
+                              <img src="/img/dc-logo.png" alt="" />
+                           </div>
+                           <div className="card_title">
+                              <p>ciao ciao</p>
+                           </div>
+                        </div>
+                        <div className="card">
+                           <div className="card_img">
+                              <img src="/img/dc-logo.png" alt="" />
+                           </div>
+                           <div className="card_title">
+                              <p>ciao ciao</p>
+                           </div>
+                        </div>
+                        <div className="card">
+                           <div className="card_img">
+                              <img src="/img/dc-logo.png" alt="" />
+                           </div>
+                           <div className="card_title">
+                              <p>ciao ciao</p>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div>
+                     <button>LOAD MORE</button>
+                  </div>
+               </div>
             </div>
          </section>
          <section className="dc_info">
